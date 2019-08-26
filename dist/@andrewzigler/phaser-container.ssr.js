4fc847d0653d3138588b3ce914b320a35ba6643a
Andrew Zigler <andrewzigler@gmail.com>
2019-08-26 16:35:31 -0700

    Build initial working component
    - handle async Phaser asset loading outside of component

---
+++ dist/@andrewzigler/phaser-container.ssr.js
@@ -0,0 +1,171 @@
+'use strict';Object.defineProperty(exports,'__esModule',{value:true});function _interopDefault(e){return(e&&(typeof e==='object')&&'default'in e)?e['default']:e}var Vue=_interopDefault(require('vue'));//
+//
+//
+//
+
+var script = {
+  name: 'PhaserContainer',
+
+  props: {
+    phaserProps: {
+      type: Object,
+      default: function default$1() {
+        return {
+          clientWidth: 800,
+          clientHeight: 600,
+          pageContainer: 'container',
+          phaserContainer: 'phaser-container'
+        }
+      }
+    },
+    game: {
+      default: function default$2() {
+        return {}
+      }
+    }
+  },
+  
+  mounted: function mounted() {
+    var this$1 = this;
+
+    this.$nextTick(function () { return this$1.game.launch({
+        width:
+          document.getElementsByClassName(this$1.phaserProps.pageContainer)[0]
+            .clientWidth < this$1.phaserProps.clientWidth
+            ? document.getElementsByClassName(this$1.phaserProps.pageContainer)[0]
+                .clientWidth
+            : this$1.phaserProps.clientWidth,
+        height:
+          document.getElementsByClassName(this$1.phaserProps.pageContainer)[0]
+            .clientHeight < this$1.phaserProps.clientHeight
+            ? document.getElementsByClassName(this$1.phaserProps.pageContainer)[0]
+                .clientHeight
+            : document.getElementsByClassName(this$1.phaserProps.pageContainer)[0]
+                .clientWidth *
+                0.7 <
+              this$1.phaserProps.clientHeight
+            ? document.getElementsByClassName(this$1.phaserProps.pageContainer)[0]
+                .clientWidth * 0.7
+            : this$1.phaserProps.clientHeight,
+        parent: this$1.phaserProps.phaserContainer
+      }); }
+    );
+  }
+};function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
+/* server only */
+, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+  if (typeof shadowMode !== 'boolean') {
+    createInjectorSSR = createInjector;
+    createInjector = shadowMode;
+    shadowMode = false;
+  } // Vue.extend constructor export interop.
+
+
+  var options = typeof script === 'function' ? script.options : script; // render functions
+
+  if (template && template.render) {
+    options.render = template.render;
+    options.staticRenderFns = template.staticRenderFns;
+    options._compiled = true; // functional template
+
+    if (isFunctionalTemplate) {
+      options.functional = true;
+    }
+  } // scopedId
+
+
+  if (scopeId) {
+    options._scopeId = scopeId;
+  }
+
+  var hook;
+
+  if (moduleIdentifier) {
+    // server build
+    hook = function hook(context) {
+      // 2.3 injection
+      context = context || // cached call
+      this.$vnode && this.$vnode.ssrContext || // stateful
+      this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext; // functional
+      // 2.2 with runInNewContext: true
+
+      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+        context = __VUE_SSR_CONTEXT__;
+      } // inject component styles
+
+
+      if (style) {
+        style.call(this, createInjectorSSR(context));
+      } // register component module identifier for async chunk inference
+
+
+      if (context && context._registeredComponents) {
+        context._registeredComponents.add(moduleIdentifier);
+      }
+    }; // used by ssr in case component is cached and beforeCreate
+    // never gets called
+
+
+    options._ssrRegister = hook;
+  } else if (style) {
+    hook = shadowMode ? function () {
+      style.call(this, createInjectorShadow(this.$root.$options.shadowRoot));
+    } : function (context) {
+      style.call(this, createInjector(context));
+    };
+  }
+
+  if (hook) {
+    if (options.functional) {
+      // register for functional component in vue file
+      var originalRender = options.render;
+
+      options.render = function renderWithStyleInjection(h, context) {
+        hook.call(context);
+        return originalRender(h, context);
+      };
+    } else {
+      // inject component registration as beforeCreate hook
+      var existing = options.beforeCreate;
+      options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+    }
+  }
+
+  return script;
+}
+
+var normalizeComponent_1 = normalizeComponent;/* script */
+var __vue_script__ = script;
+
+/* template */
+var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{attrs:{"id":_vm.phaserProps.phaserContainer}},[])};
+var __vue_staticRenderFns__ = [];
+
+  /* style */
+  var __vue_inject_styles__ = undefined;
+  /* scoped */
+  var __vue_scope_id__ = undefined;
+  /* module identifier */
+  var __vue_module_identifier__ = "data-v-cfe7de2c";
+  /* functional template */
+  var __vue_is_functional_template__ = false;
+  /* style inject */
+  
+  /* style inject SSR */
+  
+
+  
+  var component = normalizeComponent_1(
+    { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+    __vue_inject_styles__,
+    __vue_script__,
+    __vue_scope_id__,
+    __vue_is_functional_template__,
+    __vue_module_identifier__,
+    undefined,
+    undefined
+  );Vue.config.productionTip = false;
+
+new Vue({
+  render: function (h) { return h(component); },
+});exports.default=component;
--- src/PhaserContainer.vue
+++ src/PhaserContainer.vue
@@ -1,8 +1,5 @@
 <template>
-  <div v-if="downloaded" :id="phaserProps.phaserContainer" />
-  <div v-else class="placeholder">
-    Downloading ...
-  </div>
+  <div :id="phaserProps.phaserContainer" />
 </template>
 
 <script>
@@ -16,25 +13,21 @@ export default {
         return {
           clientWidth: 800,
           clientHeight: 600,
-          pageContainer: 'example container',
-          phaserContainer: 'phaser-container',
-          srcFile: 'game'
+          pageContainer: 'container',
+          phaserContainer: 'phaser-container'
         }
+      }
+    },
+    game: {
+      default() {
+        return {}
       }
     }
   },
-
-  data() {
-    return {
-      downloaded: false
-    }
-  },
-  async mounted() {
-    const game = await import(`@/assets/phaser/${this.phaserProps.srcFile}`)
-    this.downloaded = true
-
+  
+  mounted() {
     this.$nextTick(() =>
-      game.launch({
+      this.game.launch({
         width:
           document.getElementsByClassName(this.phaserProps.pageContainer)[0]
             .clientWidth < this.phaserProps.clientWidth
@@ -59,14 +52,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.placeholder {
-  font-size: 2rem;
-  font-family: 'Courier New', Courier, monospace;
-}
-
-canvas {
-  border-radius: 1rem;
-}
-</style>
